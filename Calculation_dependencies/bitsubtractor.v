fd57b050a68ee2e043e0e3d8edfcb695c02b51b0
laurenstiefel <98533788+laurenstiefel@users.noreply.github.com>
2022-11-29 23:55:50 -0500

    Create bitsubtractor.v

---
+++ Calculation_dependencies/bitsubtractor.v
@@ -0,0 +1,26 @@
+module Subtraction_txt(
+	input A_in,
+	input B_in,
+	input Carry_in,
+	output Carry_out,
+	output Diff_out
+);
+
+wire X10, X11, X12, X13, X14;
+
+
+xor U5 (Diff_out,A_in, B_in, Carry_in);
+
+or U9 (Carry_out,X10, X11, X12);
+
+and U10 (X10,B_in, Carry_in);
+
+and U11 (X11,X13, Carry_in);
+
+and U12 (X12,X14, B_in);
+
+not U13 (X13,A_in);
+
+not U14 (X14,A_in);
+
+endmodule
